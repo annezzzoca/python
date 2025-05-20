226311b4349e6eca92922716f10a08ea8affabdf
annezzoca <annelise.ferreira.silva@escola.pr.gov.br>
2025-05-20 11:44:51 -0300

    exemplo02valoresetipos

---
+++ pythonProject/capitulo1/exemplo02valoresetipos.py
@@ -0,0 +1,16 @@
+# tipos inteiros1,2,3... -2,-3...
+inteiro = 2
+print(type(inteiro))
+
+# tipo real ou ponto flutuante float 1.3 0.6... -3.056...
+float = 1.3
+print(type(float))
+
+# characteres
+char1 = '8'
+char2 = '2'
+print(type(char1))
+
+# string conjunto de characteres
+stringnome = "anne silva"
+print(type(stringnome))
