ec88c0a46c73e333bd3b32c11236a622332a76d9
annezzoca <annelise.ferreira.silva@escola.pr.gov.br>
2025-06-17 11:51:13 -0300

    exercicio 01

---
+++ pythonProject/capitulo3/correção lista 01/exercicio01.py
@@ -0,0 +1,24 @@
+def adicao(numero, numero2):
+    return numero + numero2
+
+def subtracao(numero, numero2):
+    return numero - numero2
+
+def multiplicacao (numero, numero2):
+    return numero * numero2
+numero = int(input("Digite o primeiro numero:"))
+numero2 = int(input("Digite o segundo numero:"))
+
+def divisao (numero, numero2):
+    return numero / numero2
+
+
+resultadoAdicao = adicao(numero, numero2)
+resultadoSubtracao = subtracao(numero, numero2)
+resultadoMultiplicacao = multiplicacao(numero, numero2)
+resultadoDivisao = divisao(numero, numero2)
+
+print("Adicao: ", resultadoAdicao)
+print("Subtracao ", resultadoSubtracao)
+print("Multiplicacao ", resultadoMultiplicacao)
+print("Divisao ", resultadoDivisao)
