52f99071ec45bd94dc753a1275119434b1ebaf9e
jose <rodriguez.jose@escola.pr.gov.br>
2025-09-16 11:49:20 -0300

    exercicios

---
+++ pythonProject/capitulo5/exemplo01.py
@@ -0,0 +1,10 @@
+   def validalogin(nome, senha):
+       if(nome == "Annezzoca and senha == "senha123"):
+       return print("Seja bem vindo", nome, senha)
+    else:
+       return
+
+print("===Digite seu nome===")
+nome = input()
+print("==Digite sua senha==")
+senha = input()
