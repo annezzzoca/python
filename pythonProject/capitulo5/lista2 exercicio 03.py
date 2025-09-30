a2fe720d41f57d713af2b59a5d61939229f816a0
jose <rodriguez.jose@escola.pr.gov.br>
2025-09-30 11:52:49 -0300

    lista2 exercicios

---
+++ pythonProject/capitulo5/lista2 exercicio 03.py
@@ -0,0 +1,12 @@
+n1 = int(input("Digite o primeiro numero inteiro:"))
+n2 = int(input("Digite o segundo numero inteiro:"))
+n3 = int(input("Digite o terceiro numero inteiro:"))
+
+if n1>n2 and n1>n3:
+    print("O primeiro numero inteiro é maior!")
+
+if n2>n1 and n2>n3:
+    print("O segundo numero inteiro é maior!")
+
+if n3>n1 and n3>n2:
+    print("O terceiro numero inteiro é maior!")
